9b92fdf0695561987dd6bda441da62506ff40993
Valefer <vfernandezg@unicauca.edu.co>
2023-06-10 14:52:41 -0500

    actualizacion, se hizo el login

--- feedback/Main.cs
+++ feedback/Main.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Streaming
 {
     public partial class Main : Form
     {
+        private string cadenaConexion = "Data Source=localhost;User ID=admin;Password=admin123";
+
         public Main()
         {
             InitializeComponent();
@@ -73,8 +76,44 @@ namespace Streaming
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            //Comprobar que tiene una cuenta
-            openForms(new InicioCliente());
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            // Verificar las credenciales en la base de datos
+            if (VerificarCredenciales(username, password))
+            {
+                // Credenciales válidas, el inicio de sesión es exitoso
+                MessageBox.Show("Inicio de sesión exitoso");
+                // Abre la siguiente ventana o realiza las acciones necesarias después del inicio de sesión exitoso
+                lblRegister.ForeColor = System.Drawing.Color.FromArgb(196, 110, 56);
+                openForms(new InicioCliente());
+            }
+            else
+            {
+                // Credenciales inválidas, muestra un mensaje de error o realiza las acciones correspondientes
+                MessageBox.Show("Credenciales inválidas");
+            }
         }
+
+        private bool VerificarCredenciales(string username, string password)
+        {
+            using (OracleConnection miConexion = new OracleConnection(cadenaConexion))
+            {
+                miConexion.Open();
+
+                string consulta = "SELECT COUNT(*) FROM CLIENTE WHERE NOMBRE_USUARIO_CLIENTE = :username AND CONTRASENIA = :password";
+
+                using (OracleCommand comando = new OracleCommand(consulta, miConexion))
+                {
+                    comando.Parameters.Add(":username", OracleDbType.Varchar2).Value = username;
+                    comando.Parameters.Add(":password", OracleDbType.Varchar2).Value = password;
+
+                    int count = Convert.ToInt32(comando.ExecuteScalar());
+
+                    return count > 0;
+                }
+            }
+        }
+
     }
 }
