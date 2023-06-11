58c7e9b0fe8c4b6ce3e4211b78607cde4f6eb608
Parzivak <bherrera@unicauca.edu.co>
2023-06-10 20:56:54 -0500

    clase cliente con contructor, getters and setters

--- logica/Cliente.cs
+++ logica/Cliente.cs
@@ -1,11 +1,27 @@
 ﻿using Streaming.connection;
 using System.Data;
-
+using System.Windows.Forms;
 
 namespace Streaming.logica
 {
     internal class cliente
     {
+
+
+        public cliente(string user, string pName, string sName, string pApellido, string sapellido, string password, string fNacimiento, string telefono, string correo)
+        {
+            this.user = user;
+            this.pName = pName;
+            this.sName = sName;
+            this.pApellido = pApellido;
+            this.sapellido = sapellido;
+            this.password = password;
+            this.fNacimiento = fNacimiento;
+            this.telefono = telefono;
+            this.correo = correo;
+        }
+
+
         Datos dt = new Datos();
 
         public int ingresarRegistro(string usuario, string primernombre, string segundonombre, string primerapellido,
@@ -37,6 +53,69 @@ namespace Streaming.logica
             miDs = dt.ejecutarSELECT(consulta);
             return miDs;
         }
+        private string user;
+        public string User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+
+        private string pName;
+        public string PName
+        {
+            get { return pName; }
+            set { pName = value; }
+        }
+
+        private string sName;
+        public string SName
+        {
+            get { return sName; }
+            set { sName = value; }
+        }
+
+        private string pApellido;
+        public string PApellido
+        {
+            get { return pApellido; }
+            set { pApellido = value; }
+        }
+
+        private string sapellido;
+        public string SApellido
+        {
+            get { return sapellido; }
+            set { sapellido = value; }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        private string fNacimiento;
+        public string FNacimiento
+        {
+            get { return fNacimiento; }
+            set { fNacimiento = value; }
+        }
+
+        private string telefono;
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        private string correo;
+        public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
+
 
     }
 }
